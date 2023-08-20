09e7bce9558a88238e6917f7b0260fc1fbc29630
LB-Brandon <rmsdud623@gmail.com>
2023-08-19 23:50:08 -0700

    update

--- routes/recipe.js
+++ routes/recipe.js
@@ -4,6 +4,6 @@ const recipeController = require('../controllers/recipeController');
 
 router.get('/mainrecipes', recipeController.getRecipes);
 // 가진 재료, 없는 재료 조회
-router.get('/seewhatyougot', userController.seeWhatYouGot);
+router.get('/seewhatyougot', recipeController.seeWhatYouGot);
 
 module.exports = router;
