3b02c1d622bde2685147b831cb88988ebdd7692d
LB-Brandon <rmsdud623@gmail.com>
2023-08-23 10:36:01 -0700

    update

--- routes/recipe.js
+++ routes/recipe.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const recipeController = require('../controllers/recipeController');
 
 router.get('/mainrecipes', recipeController.getRecipes);
+
+router.get('/subrecipes', recipeController.getSubRecipes);
 // 가진 재료, 없는 재료 조회
 router.get('/seewhatyougot', recipeController.seeWhatYouGot);
 
